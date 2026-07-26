d5c2e7366eee9bfe946d8fe4d8cd2eacd433496a
thedougshelton <dougedwardshelton@gmail.com>
2026-07-26 18:22:24 -0400

    Fix vertical OCR photo framing

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "packrat-inventory-v5-runtime-35";
+const CACHE_NAME = "packrat-inventory-v5-runtime-36";
 const APP_PATHS = [
   "./",
   "./index.html",
-  "./ocr-priority-patch.js?v=35",
-  "./ocr-hard-rules.js?v=35",
+  "./ocr-priority-patch.js?v=36",
+  "./ocr-hard-rules.js?v=36",
   "./vendor/xlsx.bundle.js",
   "./vendor/tesseract.min.js",
   "./vendor/tesseract-worker.min.js",
